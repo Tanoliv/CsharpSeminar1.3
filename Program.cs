572034312d7e01879eb1f5877595b161924fa800
Oleg <tmr777@mail.ru>
2022-10-05 14:14:11 +0300

    Создание ряда чётных чисел

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Создание числового ряда от 1 до N.
+
+Console.WriteLine("Создание числового ряда от 1 до числа N");
+
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 1;  i < n; i++){
+    
+    if(i % 2 == 0){
+      
+ Console.Write($"{i}, ");
+     }  
+      
+}
+
+
